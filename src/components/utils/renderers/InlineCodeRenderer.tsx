f9258818f5c0d36c28dfdfe9b8850927969b3d19
Daryl Cecile <darylcecile@gmail.com>
2025-12-04 22:26:36 +0000

    Update background color in InlineCodeRenderer for improved visibility

--- src/components/utils/renderers/InlineCodeRenderer.tsx
+++ src/components/utils/renderers/InlineCodeRenderer.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from "geist/font/mono";
 export function InlineCodeRenderer({ children }: { children: React.ReactNode }) {
 	return (
 		<span className={cn(GeistMono.className, 'text-foreground relative  whitespace-nowrap')}>
-			<span className="z-0 block absolute mix-blend-soft-light bg-black dark:bg-white -top-0.5 left-0 -bottom-0.5 right-0 rounded" />
+			<span className="z-0 block absolute dark:mix-blend-soft-light bg-purple-500/10 dark:bg-white -top-0.5 left-0 -bottom-0.5 right-0 rounded" />
 			<span className={"px-1 py-0.5 z-1 relative text-sm whitespace-break-spaces"}>{children}</span>
 		</span>
 	)
